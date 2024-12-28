e5ff9280b0a0080c5d376a737cd3436b68e29bb8
Palmier-Ignacio <palmier.ignacio.nicolas@gmail.com>
2024-12-27 23:11:29 -0300

    test

--- src/main/java/net/naacho/Prueba01Mod/Prueba01Mod.java
+++ src/main/java/net/naacho/Prueba01Mod/Prueba01Mod.java
@@ -19,7 +19,7 @@ public class Prueba01Mod
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "prueba01";
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
     public Prueba01Mod()
     {
